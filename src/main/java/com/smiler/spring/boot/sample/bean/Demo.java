329e5d92444acb5222a80a94cff069d8da4f783a
xile.su <xile.su@nio.com>
2018-08-30 15:27:05 +0800

    Update: jpa

--- src/main/java/com/smiler/spring/boot/sample/bean/Demo.java
+++ src/main/java/com/smiler/spring/boot/sample/bean/Demo.java
@@ -2,17 +2,22 @@ package com.smiler.spring.boot.sample.bean;
 
 import com.alibaba.fastjson.annotation.JSONField;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import lombok.Data;
 
 /**
  * Created by xile.su on 2018/8/29
  */
 @Data
+@Entity
 public class Demo {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
-    @JSONField(format = "yyyy-MM-dd-HH:mm:ss")
     private Date createTime;
     private Date updateTime;
 }
